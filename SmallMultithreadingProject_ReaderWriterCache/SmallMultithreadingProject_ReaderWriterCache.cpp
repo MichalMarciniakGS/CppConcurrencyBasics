8932ca4f98b0708cd93f21c14a4b10c4bfb74c00
MichalMarciniakGS <michal1998pl@gmail.com>
2026-06-12 22:58:36 +0200

    - Added shared mutex to cache class to properly introduce locks to methods in cache class - Created some jthreads to test how get and put work on multiple threads

--- SmallMultithreadingProject_ReaderWriterCache/SmallMultithreadingProject_ReaderWriterCache.cpp
+++ SmallMultithreadingProject_ReaderWriterCache/SmallMultithreadingProject_ReaderWriterCache.cpp
@@ -2,12 +2,17 @@
 #include <map>
 #include <optional>
 #include <string>
+#include <shared_mutex>
+#include <vector>
+#include <thread>
+#include <syncstream>
 
 class Cache {
 
 public:
 
-    std::optional<std::string> get(const std::string& key) {
+    std::optional<std::string> get(const std::string& key) const {
+        std::shared_lock lock(mtx_);
         auto it = cache_.find(key);
         if (it != cache_.end()) {
             return it->second;
@@ -16,10 +21,12 @@ public:
     }
 
     void put(std::string key, std::string value) {
+        std::unique_lock lock(mtx_);
         cache_.insert_or_assign(std::move(key),std::move(value));
     }
 
     void remove(const std::string& key) {
+        std::unique_lock lock(mtx_);
         cache_.erase(key);
     }
 
@@ -27,20 +34,32 @@ public:
 
 private:
     std::map<std::string, std::string> cache_;
+    mutable std::shared_mutex mtx_;
 
 };
 
 int main()
 {
     Cache c;
+    
+    std::vector<std::jthread> jthreads;
+    for (size_t i{}; i < 5; ++i) {
+        jthreads.emplace_back([&,i]() {
+            c.put( "key" + std::to_string(i), "val" + std::to_string(i));
+            std::osyncstream(std::cout) << "Current writer thread id is: " << std::this_thread::get_id() << "\n";
+            });
+    }
 
-    c.put("name", "Alice");
-    std::cout << c.get("name").value_or("not found") << "\n";
+    for (auto& t : jthreads) {
+        t.join();
+    }
+    jthreads.clear();
 
-    c.put("name", "Bob");
-    std::cout << c.get("name").value_or("not found") << "\n";
-
-    c.remove("name");
-    std::cout << c.get("name").value_or("not found") << "\n";
+    for (size_t i{}; i < 6; ++i) {
+        jthreads.emplace_back([&,i]() {
+            const auto currVal = c.get("key" + std::to_string(i));
+            std::osyncstream(std::cout) << "Current reader thread id is: " << std::this_thread::get_id() << ", it's value is: " << currVal.value_or("not found") << "\n";
+            });
+    }
 
 }
