3a04b247bc32141b4b44acd31281c5240a851166
MichalMarciniakGS <michal1998pl@gmail.com>
2026-06-21 15:37:21 +0200

    - Tested promise/future approach instead of condition_variable one - Removed not needed code (mutex, condition_variable, locks) for current approach

--- SmallMultithreadingProject_OneShotSignaling/SmallMultithreadingProject_OneShotSignaling.cpp
+++ SmallMultithreadingProject_OneShotSignaling/SmallMultithreadingProject_OneShotSignaling.cpp
@@ -1,47 +1,32 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-#include <condition_variable>
+#include <future>
 
 using namespace std::chrono_literals;
 
-std::mutex mtx;
-std::condition_variable condVar;
-bool ready = false;
-
-void workerFunction() {
-    std::cout << "Worker: waiting for start signal \n";
-
-    std::unique_lock<std::mutex> lock(mtx);
-    condVar.wait(lock, []() {return ready;});
-
-    std::cout << "Worker: received start signal, doing work \n";
+void workerFunction(std::future<void> startFuture) {
+    std::cout << "Worker: waiting for start signal\n";
+    startFuture.wait();
+    std::cout << "Worker: received start signal, doing work\n";
 }
 
 int main()
 {
+    std::promise<void> startSignal;
+    std::future<void> startFuture = startSignal.get_future();
 
     std::cout << "Main: Starting with creating workerThread \n";
 
-    //Spawn worker thread
-    std::thread workerThread(workerFunction);
+    std::cout << "Main: creating worker thread\n";
+    std::thread workerThread(workerFunction, std::move(startFuture));
 
-    //Worker blocks
-    std::cout << "Main: preparing... \n";
+    std::cout << "Main: preparing...\n";
     std::this_thread::sleep_for(1s);
 
-    //Main thread sends signal
-    std::cout << "Main: sending start signal \n";
-    {
-        std::lock_guard<std::mutex> lock(mtx);
-        ready = true;
-    }
+    std::cout << "Main: sending start signal\n";
+    startSignal.set_value();
 
-    //Worker proceeds
-    condVar.notify_one();
-
-    //Main thread joins the worker
     workerThread.join();
-
-    std::cout << "Main: worker finished. exiting \n";
+    std::cout << "Main: worker finished. exiting\n";
 }
